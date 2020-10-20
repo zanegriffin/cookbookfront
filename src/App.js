6d02da8ebaf6d16f9be13126a0a0b01ab183e99c
Zane <zanebgriffin@gmail.com>
2020-10-19 22:48:14 -0500

    rendered axios call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const url = "https://cookbookapizane.herokuapp.com/api"
+  const [authors, setAuthors] = React.useState([])
+  //get all author
+  const getAuthors = () => {
+    axios.get(url + '/authors')
+    .then((res) => {
+      const persons = res.data
+      console.log(res.data.data)
+      setAuthors(persons.data)
+    })
+    .catch((error) => console.log(error))
+  }
+  React.useEffect(() => getAuthors(), [])
+  
+  const renderAuthors = authors.map((author) => {
+    return(
+      <>
+        <h3>{author.firstName} {author.lastName}</h3>
+        <h3>{author._id}</h3>
+      </>
+    )
+  })
+  
+  //get all cookbook
+  axios({url: url + '/cookbooks'})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Cookbooks</h1>
       </header>
+      <p>{authors.length > 0 ? renderAuthors : ''}</p>
+
     </div>
   );
 }
